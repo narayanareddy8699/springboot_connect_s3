951ac9d214d343ddd1de99d2b856bd046e3aff08
narayana y <Nyerragu@meredith.com>
2019-07-19 17:22:58 +0530

    firstcommit

---
+++ src/main/java/com/springaws/springconnects3/Springconnects3Application.java
@@ -0,0 +1,13 @@
+package com.springaws.springconnects3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springconnects3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springconnects3Application.class, args);
+	}
+
+}
